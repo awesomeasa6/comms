5724d05a87e17f09854de36b29f99af43e932051
Asa Hafele <asahafele@outlook.com>
2018-08-28 11:28:24 -0400

    Initial commit

---
+++ lua/autorun/comms_cfg.lua
@@ -0,0 +1,15 @@
+if SERVER then
+	cfgComms = {}
+	cfgComms.tali = true
+	cfgComms.us = true
+	cfgComms.colorUs = {
+		"255", --Red
+		"255", --Green
+		"255" --Blue
+	}
+	cfgComms.colorTali = {
+		"255", --Red
+		"255", --Green
+		"255" --Blue
+	}
+end
